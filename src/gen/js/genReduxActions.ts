a4ceca2f04d1a54562bbc8b731482c4ae69ba224
Ville Lahdenvuo <ville.lahdenvuo@grano.fi>
2019-11-05 13:49:01 +0100

    feat(redux): Expose req options in *_START actions

--- src/gen/js/genReduxActions.ts
+++ src/gen/js/genReduxActions.ts
@@ -58,7 +58,7 @@ ${isTs ? `export type ${actionComplete} = ${returnType}${ST}`: ''}
 
 export function ${op.id}(${paramSignature})${isTs? ': any' : ''} {
   return dispatch => {
-    dispatch({ type: ${actionStart}, meta: { info } })${ST}
+    dispatch({ type: ${actionStart}, meta: { info, options } })${ST}
     return ${op.group}.${op.id}(${params})
       .then(response => dispatch({
         type: ${actionComplete},
